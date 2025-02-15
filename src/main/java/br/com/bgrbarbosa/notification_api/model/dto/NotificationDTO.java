c095574153ba8a2ca4c1606e28af6e1e9b549e24
bgrbarbosadev <bgrbarbosa.dev@gmail.com>
2025-02-15 10:31:18 -0300

    Configuração de mensagens padrão através de arquivos properties

--- src/main/java/br/com/bgrbarbosa/notification_api/model/dto/NotificationDTO.java
+++ src/main/java/br/com/bgrbarbosa/notification_api/model/dto/NotificationDTO.java
@@ -3,10 +3,7 @@ package br.com.bgrbarbosa.notification_api.model.dto;
 
 import br.com.bgrbarbosa.notification_api.model.enums.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
@@ -15,25 +12,25 @@ public record NotificationDTO(
 
         Long id,
 
-        @Email(message = "Invalid email!")
+        @Email(message = "{email.message}")
         String email,
 
-        @NotBlank(message = "Phone field cannot be empty")
+        @NotBlank(message = "{not.blank.message}")
         String tel,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime dateTimeShipping,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-        @NotNull(message = "Enter a date")
+        @NotNull(message = "{not.null.message}")
         LocalDateTime dateTimeScheduling,
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime dateTimeModify,
 
-        @NotEmpty(message = "Message must be completed")
+        @NotEmpty(message = "{not.empty.message}")
+        @Size(min = 10, max = 100, message = "{size.message}")
         String message,
 
-        Status statusNotification
-) {
+        Status statusNotification) {
 }
